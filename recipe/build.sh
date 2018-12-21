36c27f61f68faba7002e81e12a82cbaa8e116e49
Chilipp <p.sommer@posteo.de>
2018-12-21 13:14:20 +0100

    Skip py27 and do not remove psyplot-gui app

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-$PYTHON setup.py install --single-version-externally-managed --record record.txt
-
-# delete the Psyplot.app. This should not be included in the conda-forge package
-rm -rf psyplot_gui/app/Psyplot.app
-
+$PYTHON -m pip install . --no-deps --ignore-installed -vvv
